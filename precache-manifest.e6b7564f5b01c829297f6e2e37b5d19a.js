dbeb4a5e67ab3a579805bac437c16a80466fd7c7
Xinghui Lu <c20090204@gmail.com>
2020-01-26 16:42:31 -0800

    Updates

---
+++ precache-manifest.e6b7564f5b01c829297f6e2e37b5d19a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf618578fededf306d766289ac575546",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "238c55636b14010f3dce",
+    "url": "/site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d206b5b00d86697ea37",
+    "url": "/site/static/js/2.08d98e58.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/site/static/js/2.08d98e58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "238c55636b14010f3dce",
+    "url": "/site/static/js/main.2c25bc0e.chunk.js"
+  },
+  {
+    "revision": "bb67c388ea26c15d3d85",
+    "url": "/site/static/js/runtime-main.6243b492.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
